188909e58d7aec7442276a7782172632a2ba38a1
adembayram <123821723+adembayram@users.noreply.github.com>
2023-07-20 18:04:35 +0300

    Update AppServiceProvider.php
    AppServiceProvider edit.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,32 +37,13 @@ class AppServiceProvider extends ServiceProvider
     {
 
         Paginator::useBootstrap();
-        //
-
-//        View::composer(['admin.*'],function ($view){
-//
-//            $endDate =now()->addMinutes(10);
-//            $statistics = Cache::remember('statistics',$endDate,function (){
-//
-//                return [
-//                    'pending_order' => Order::where('status','Siparişiniz alındı')->count(),
-//                    'completed_order' => Order::where('durum', 'Sipariş tamamlandı')->count(),
-//                    'total_products' => Product::count(),
-//                    'total_category' => Category::count(),
-//                    'total_users' => User::count()
-//
-//                ];
-//            });
-//
-//            $view->with('statistics',$statistics);
-
-//        });
-
-         $settings = Setting::first();
-          $menus = Menu::all();
+        
+        /*
+        $settings = Setting::first();
+        $menus = Menu::all();
         
          view()->share(compact('settings','menus'));
-
+        */
 
 
 
